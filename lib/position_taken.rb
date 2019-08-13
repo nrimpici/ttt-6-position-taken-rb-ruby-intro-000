4f07786a1adcabd7b72a95663e245e7b7d4e8135
Nicholas Rimpici <nrimpici@gmail.com>
2019-08-13 01:56:34 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == "  "
-  elsif board[index] == "X"
-  else board[index] == "O"
+    return false
+  else
+    return true
   end
 end
